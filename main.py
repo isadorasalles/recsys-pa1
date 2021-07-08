5afb00c5e8d5aa8a585e7a19814c761dad3bc345
isadorasalles <isadorasalles@dcc.ufmg.br>
2021-07-08 17:58:37 -0300

    best submission + modularization

--- main.py
+++ main.py
@@ -1,40 +1,13 @@
 import sys
 import pandas as pd
 import numpy as np
-
-ratings_path = sys.argv[1]
-targets_path = sys.argv[2]
-
-df_ratings = pd.read_csv(ratings_path)
-df_ratings[['UserId', 'ItemId']] = pd.DataFrame(df_ratings["UserId:ItemId"].str.split(':',1).tolist(),
-                                                columns = ['UserId','ItemId'])
-
-df_targets = pd.read_csv(targets_path)
-df_targets[['UserId', 'ItemId']] = pd.DataFrame(df_targets["UserId:ItemId"].str.split(':',1).tolist(),
-                                                columns = ['UserId','ItemId'])
-
-list_user = df_ratings["UserId"].tolist()
-list_user_t = df_targets["UserId"].tolist()
-list_items = df_ratings["ItemId"].tolist()
-list_items_t = df_targets["ItemId"].tolist()
-
-unique_users = list(set(list_user))
-unique_items = list(set(list_items))
-
-for i in list_user_t:
-    if i not in list_user:
-        print("puuuts")
- 
-# matrix = np.zeros((len(unique_users), len(unique_items)))
-
-# for i, user in enumerate(unique_users):
-#     # print(user)
-#     for j, item in enumerate(unique_items):
-#         if user+":"+item in df_ratings["UserId:ItemId"]: 
-#             print("ola")
-#             print(df_ratings[df_ratings["UserId:ItemId"] == user+":"+item])
-
-#df = pd.DataFrame(columns=unique_items, index=unique_users)
+from svd import svd
 
 
-# print(df.head())
+if __name__ == "__main__":
+    ratings_path = sys.argv[1]
+    targets_path = sys.argv[2]
+    svd_ = svd(epochs = 20, lr = 0.005, n_factors=5, reg=0.1)
+    svd_.read_ratings(ratings_path)
+    svd_.stochastic_gradient()
+    svd_.submission(targets_path)
--- svd.py
+++ svd.py
@@ -3,29 +3,12 @@ import pandas as pd
 
 class svd():
 
-    def __init__(self, ratings_path, targets_path):
-        # self.ratings = pd.read_csv(ratings_path)
-        # self.ratings[['UserId', 'ItemId']] = pd.DataFrame(self.ratings["UserId:ItemId"].str.split(':',1).tolist(),
-        #                                                 columns = ['UserId','ItemId'])
-
-        # self.targets = pd.read_csv(targets_path)
-        # self.targets[['UserId', 'ItemId']] = pd.DataFrame(self.targets["UserId:ItemId"].str.split(':',1).tolist(),
-        #                                                 columns = ['UserId','ItemId'])
-
-        # list_user = self.ratings["UserId"].tolist()
-        # #list_user.extend(self.targets["UserId"].tolist())
-        # list_items = self.ratings["ItemId"].tolist()
-        # #list_items.extend(self.targets["ItemId"].tolist())
-
-        # self.unique_users = list(set(list_user))
-        # self.unique_items = list(set(list_items))
-        # self.users_index = list(range(0, len(self.unique_users)-1))
-        # self.items_index = list(range(0, len(self.unique_items)-1))
-        self.read_ratings(ratings_path)
-        self.stochastic_gradient()
-        self.submission(targets_path)
-
-        ## pegar o index antes, já deixar uma tupla com index_user, index_item, prediction
+    def __init__(self, epochs = 20, lr = 0.005, n_factors=5, reg=0.1):
+        ## best configuration
+        self.epochs = epochs
+        self.lr = lr
+        self.n_factors = n_factors
+        self.reg = reg
 
     def read_ratings(self, ratings_path):
         df = pd.read_csv(ratings_path)
@@ -45,37 +28,32 @@ class svd():
                 i_ind+=1
             self.ratings.append((self.user_index[userid], self.item_index[itemid], float(row["Prediction"])))
         self.average_rating = df["Prediction"].mean()
-        # print("User ratings saved!")
 
-    def predict(self, u, i, n_features):
+    def predict(self, u, i):
         pred = 0.0
-        for f in range(n_features):
+        for f in range(self.n_factors):
             pred += self.P[u][f] * self.Q[f][i]
         return pred
 
-    # melhor setagem= steps: 20, n_features:5, reg=0.01 
-    # pensar em mudar a inicialização
-    def stochastic_gradient(self, steps=20, alpha=0.005, n_features=5, reg=0.02):
-        self.P = np.ones((len(self.user_index), n_features))
-        self.Q = np.ones((len(self.item_index), n_features))
+    # melhor setagem= steps: 20, n_features:5, reg=0.1, alpha
+    def stochastic_gradient(self):
+        self.P = np.ones((len(self.user_index), self.n_factors))
+        self.Q = np.ones((len(self.item_index), self.n_factors))
         self.Q = self.Q.T
 
         sum_ = 0
-        for s in range(steps):
-            # print(s)
+        for s in range(self.epochs):
             rmse = 0
             for u, i, rui in self.ratings:
-                eui = pd.to_numeric(rui) -  self.predict(u, i, n_features)
+                eui = rui -  self.predict(u, i)
                 rmse += eui*eui
                 
-                for f in range(n_features):
-                    self.P[u][f] = self.P[u][f] + alpha* (2*eui*self.Q[f][i] - reg*self.P[u][f])
-                    self.Q[f][i] = self.Q[f][i] + alpha* (2*eui*self.P[u][f] - reg*self.Q[f][i])
+                for f in range(self.n_factors):
+                    self.P[u][f] = self.P[u][f] + self.lr * (2*eui*self.Q[f][i] - self.reg*self.P[u][f])
+                    self.Q[f][i] = self.Q[f][i] + self.lr * (2*eui*self.P[u][f] - self.reg*self.Q[f][i])
                 sum_+=1
                 
             rmse/=sum_
-            # print(rmse)
-        
     
     def submission(self, targets_path):
         df = pd.read_csv(targets_path)
@@ -84,15 +62,11 @@ class svd():
             userid = row["UserId:ItemId"].split(":")[0]
             itemid = row["UserId:ItemId"].split(":")[1]
             if userid in self.user_index.keys() and itemid in self.item_index.keys():
-                # print(self.P[self.user_index[userid]])
-                prediction = self.predict(self.user_index[userid], self.item_index[itemid], 5)
+                prediction = self.predict(self.user_index[userid], self.item_index[itemid])
                 if prediction > 10:
                     prediction = 10
                 elif prediction < 0:
                     prediction = 0
             else:
-                prediction = self.average_rating  # arrumar isso aqui
+                prediction = self.average_rating 
             print("{}:{},{}".format(userid, itemid, prediction))
-
-if __name__ == "__main__":
-    svd("ratings.csv", "targets.csv")
