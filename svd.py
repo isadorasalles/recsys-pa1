d1c09e28d60dc0fc8d4f77e98c732936cd1f2290
isadorasalles <isadorasalles@dcc.ufmg.br>
2021-07-07 23:11:00 -0300

    submission with score 1.70274

--- svd.py
+++ svd.py
@@ -23,6 +23,7 @@ class svd():
         # self.items_index = list(range(0, len(self.unique_items)-1))
         self.read_ratings(ratings_path)
         self.stochastic_gradient()
+        self.submission(targets_path)
 
         ## pegar o index antes, já deixar uma tupla com index_user, index_item, prediction
 
@@ -43,17 +44,8 @@ class svd():
                 self.item_index[itemid] = i_ind
                 i_ind+=1
             self.ratings.append((self.user_index[userid], self.item_index[itemid], float(row["Prediction"])))
-        print("ACABOU!")
-
-    def d0(self, v1,v2):
-        """                                                                                                     
-        d0 is Nominal approach:                                                                                 
-        multiply/add in a loop                                                                                  
-        """
-        out = 0
-        for k in range(len(v1)):
-            out += v1[k] * v2[k]
-        return out
+        self.average_rating = df["Prediction"].mean()
+        # print("User ratings saved!")
 
     def predict(self, u, i, n_features):
         pred = 0.0
@@ -61,36 +53,46 @@ class svd():
             pred += self.P[u][f] * self.Q[f][i]
         return pred
 
-    def stochastic_gradient(self, steps=5, alpha=0.005, n_features=3):
-
-        # print(self.unique_items)
-        # print(self.unique_users)
-        
-        self.P = np.random.rand(len(self.user_index), n_features)
-        self.Q = np.random.rand(len(self.item_index), n_features)
+    # melhor setagem= steps: 20, n_features:5, reg=0.01 
+    # pensar em mudar a inicialização
+    def stochastic_gradient(self, steps=20, alpha=0.005, n_features=5, reg=0.02):
+        self.P = np.ones((len(self.user_index), n_features))
+        self.Q = np.ones((len(self.item_index), n_features))
         self.Q = self.Q.T
 
-        sum_ =0
+        sum_ = 0
         for s in range(steps):
-            print(s)
+            # print(s)
+            rmse = 0
             for u, i, rui in self.ratings:
-                # u = self.unique_users.index(user)
-                # i = self.unique_items.index(item) 
-                # print(rui)
                 eui = pd.to_numeric(rui) -  self.predict(u, i, n_features)
-                # print(eui)
+                rmse += eui*eui
+                
                 for f in range(n_features):
-                    self.P[u][f] = self.P[u][f] + 2*alpha*eui*self.Q[f][i]
-                    self.Q[f][i] = self.Q[f][i] + 2*alpha*eui*self.P[u][f]
+                    self.P[u][f] = self.P[u][f] + alpha* (2*eui*self.Q[f][i] - reg*self.P[u][f])
+                    self.Q[f][i] = self.Q[f][i] + alpha* (2*eui*self.P[u][f] - reg*self.Q[f][i])
                 sum_+=1
-                print(sum_)
+                
+            rmse/=sum_
+            # print(rmse)
         
-        print(self.P)
-        print(self.Q.T)
-        print("RESULTADO")
-        #print (np.dot(self.P, self.Q))
-
-        # return P, Q.T
+    
+    def submission(self, targets_path):
+        df = pd.read_csv(targets_path)
+        print("UserId:ItemId,Prediction")
+        for _, row in df.iterrows():
+            userid = row["UserId:ItemId"].split(":")[0]
+            itemid = row["UserId:ItemId"].split(":")[1]
+            if userid in self.user_index.keys() and itemid in self.item_index.keys():
+                # print(self.P[self.user_index[userid]])
+                prediction = self.predict(self.user_index[userid], self.item_index[itemid], 5)
+                if prediction > 10:
+                    prediction = 10
+                elif prediction < 0:
+                    prediction = 0
+            else:
+                prediction = self.average_rating  # arrumar isso aqui
+            print("{}:{},{}".format(userid, itemid, prediction))
 
 if __name__ == "__main__":
-    factorization("ratings.csv", "targets.csv")
+    svd("ratings.csv", "targets.csv")
